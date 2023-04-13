8e9a01df1af0d597b74bb251925863ca7231b27d
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-04-13 01:37:13 -0400

    tests for renderBoard

--- render.js
+++ render.js
@@ -12,5 +12,28 @@ function renderLetterBoxInput({correctAns, idx}) {
 function renderLetterBox({correctAns, isEmpty, idx}) {
   if(isEmpty) {
     return `<div class="letter-box letter-box-empty"></div>`
+  } else {
+    const input = renderLetterBoxInput({correctAns, idx});
+    return `
+      <div class="letter-box">${input}</div>
+    `
   }
+}
+
+function renderBoard(crosswordData) {
+  const { cols, rows, table } = crosswordData;
+  const boxElms = [];
+  for(let i=0; i<rows; i++){
+    for(let j=0; j<cols; j++){
+      const letter = table[i][j];
+      let box;
+      if(letter === "-") {
+        box = renderLetterBox({isEmpty: true});
+      } else {
+        box = renderLetterBox({correctAns: letter, idx: i+j})
+      }
+      boxElms.push(box);
+    }
+  }
+  return boxElms.join("\n");
 }
--- test.js
+++ test.js
@@ -1,3 +1,25 @@
+
+const testData1 = {
+  "table": [
+      [ "-", "-", "-", "-", "-", "h", "-" ],
+      [ "-", "-", "-", "-", "-", "o", "-" ],
+      [ "o", "c", "t", "o", "p", "u", "s" ],
+      [ "-", "-", "-", "-", "-", "n", "-" ],
+      [ "-", "-", "-", "-", "-", "d", "-" ]
+  ],
+  "result": [
+      {
+          "clue": "a kind of dog", "answer": "hound", "startx": 6, "starty": 1, "orientation": "down", "position": 1
+      },
+      {
+          "clue": "a kind of sea animal", "answer": "octopus", "startx": 1, "starty": 3, "orientation": "across", "position": 2
+      }
+  ],
+  "rows": 5,
+  "cols": 7,
+  "table_string": "-----h-<br>-----o-<br>octopus<br>-----n-<br>-----d-<br>"
+};
+
 describe('Render Engine', function () {
   describe('#renderLetterBoxInput', function () {
     it('should render an input for the answer', function () {
@@ -12,10 +34,24 @@ describe('Render Engine', function () {
       chai.assert(letterBoxInput.match(/letter-box-input-error/));
     });
   });
+
   describe('#renderLetterBox', function () {
     it('should render an empty box', function () {
       const emptyLetterBox = renderLetterBox({isEmpty: true});
       chai.assert(emptyLetterBox.match(/letter-box-empty/));
     });
+  });
+
+  describe("#renderBoard", function () {
+    it("should render a full board", function () {
+      const gameBoard = renderBoard(testData1);
+      chai.assert(gameBoard.includes('data-correct-answer="o"'));
+      chai.assert(gameBoard.includes('data-correct-answer="c"'));
+      chai.assert(gameBoard.includes('data-correct-answer="c"'));
+      chai.assert(gameBoard.includes('data-correct-answer="t"'));
+      chai.assert(gameBoard.includes('data-correct-answer="p"'));
+      chai.assert(gameBoard.includes('data-correct-answer="u"'));
+      chai.assert(gameBoard.includes('data-correct-answer="s"'));
+    })
   });
 });
