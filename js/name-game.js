ab6e0d766ac782d0a8d97e03bafff5e7b455e4f3
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-04-18 15:21:15 -0400

    name-game: insert current-name from query string

--- js/name-game.js
+++ js/name-game.js
@@ -1,4 +1,52 @@
+function sanityCheckName(answer) {
+  if(answer.match(/^[a-zA-Z]+$/)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function removeClass(selector, className) {
+  const elms = document.querySelectorAll(selector);
+  for(let elm of elms) {
+    elm.classList.remove(className);
+  }
+}
+
+function addClass(selector, className) {
+  const elms = document.querySelectorAll(selector);
+  for(let elm of elms) {
+    elm.classList.add(className);
+  }
+}
+
+function setCurrentName(answer) {
+  // sanity check here...
+  if(!sanityCheckName(answer)){
+    console.error(`Error processing: ${answer}`);
+    throw new Error("Unknown characters in answer.");
+  }
+  const elms = document.querySelectorAll('.current-person');
+  for(let elm of elms) {
+    elm.textContent = answer;
+  }
+}
+
 // TODO: grab data from Query String
+function analyzeQueryString() {
+  const queryString = location.search;
+  const result = checkQueryStringForMissingClue(queryString);
+  if(result.isCompleted) {
+    // display link to crossword puzzle
+  } else {
+    // continue with name game
+    removeClass('#intro-container', 'hidden')
+    setCurrentName(result.nextAnswer);
+  }
+
+}
+
+analyzeQueryString();
 
 // TODO: find next name
 
