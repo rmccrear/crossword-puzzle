9d41504ef072dfea9b61fdb4a87876f815abf976
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-04-21 02:19:16 -0400

    check for invalid crossword puzzle

--- js/create-name-game.js
+++ js/create-name-game.js
@@ -13,12 +13,26 @@ function generateLinkForCrossword(crosswordData) {
 let crosswordData;
 
 
+// sometimes the algorithm fails to include everyone.
+function checkForMissing(crosswordData) {
+  const missing = crosswordData.result.filter((item) => item.orientation === "none");
+  if(missing.length > 0) {
+    return missing.map((item) => item.answer);
+  } else {
+    return false;
+  }
+}
+
 document.getElementById("create-button").addEventListener("click", function(e) {
   e.preventDefault();
   const csv = document.getElementById("create-crossword-textarea").value;
   const crosswordItems = crosswordItemsFromCSV(csv);
   // set global!!
   crosswordData = generateLayout(crosswordItems);
+  const missing = checkForMissing(crosswordData, crosswordItems);
+  if(missing) {
+    alert(`WARNING: A crossword containing all the names could not be created. This happens if the names just happen not to align in a way that allow us to create one. We suggest adding an extra name to the front of the list until you get a valid crossword puzzle. The missing names is/are: ${missing.join(", ")}`);
+  }
   const url = generateLinkForCrossword(crosswordData);
   removeClass(".after-create-instructions", "hidden");
   const link = document.getElementById("created-game-link");
